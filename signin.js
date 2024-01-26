6ce64fa13847e602f3fdc72fd9d553b3207127c3
sabbirsun <sabbirsun1920@gmail.com>
2024-01-26 19:03:02 +0600

    sign in password visible

---
+++ signin.js
@@ -0,0 +1,8 @@
+const password = document.querySelector("#password");
+const togglePassword = document.querySelector("#togglePassword");
+
+togglePassword.addEventListener("click", ()=>{
+    const type = password.getAttribute("type") == "password" ? "text":"password";
+    password.setAttribute("type", type);
+    togglePassword.classList.toggle("bi-eye");
+})
